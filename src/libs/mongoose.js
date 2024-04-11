9a2faea3c186a354147a375b180b6de921cf7c3f
rodrigo745 <roodrigoquinteros@gmail.com>
2024-04-11 10:44:27 -0300

    cacheado

--- src/libs/mongoose.js
+++ src/libs/mongoose.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function ConnectDB(){
-    await mongoose.connect(process.env.ENLACE);
+    await mongoose.connect(process.env.ENLACE, {
+        next: {revalidate: 0 }
+    });
 }
