eb922d18c93208d8f8ad445f89f47eea80cdb954
rodrigo745 <roodrigoquinteros@gmail.com>
2024-04-11 09:41:50 -0300

    nueo

--- src/libs/mongoose.js
+++ src/libs/mongoose.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export default async function ConnectDB(){
-    await mongoose.connect("mongodb+srv://rodrigoquinteros:roklee2611@cluster0.sr17qvd.mongodb.net/test-mongo");
+    await mongoose.connect("mongodb+srv://rodrigoquinteros:roklee2611@cluster0.sr17qvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
